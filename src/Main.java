4ca690c4efe3797b10a8d0a9758dcf6cfa33b801
hunter <westerhr@mail.uc.edu>
2024-11-08 11:31:50 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+    }
+}
